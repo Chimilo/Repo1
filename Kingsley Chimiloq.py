af1a8ec970969330016b252be5d1fe533f2bf7f1
Chimilo <42252867+Chimilo@users.noreply.github.com>
2018-08-12 19:46:04 +0100

    Add files via upload

---
+++ Kingsley Chimiloq.py
@@ -0,0 +1,17 @@
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:24:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+(-b + 4 * a * c) / 2 * a
+>>> ================================ RESTART ================================
+>>> 
+Enter your Name here:
+Davido Wizkid
+>>> ================================ RESTART ================================
+>>> 
+Enter your Age here:
+18 years
+>>> ================================ RESTART ================================
+>>> 
+Mr Davido, you will 100 years by the year 2080
+>>> 
